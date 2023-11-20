4a9a184387809600900be4430c63a82e07f4ec2c
Faizan Naseer <111910355+faizannaseerr@users.noreply.github.com>
2023-11-20 17:00:18 +0000

    Add files via upload

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,21 +1,50 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <Link to="/">
+  const location = useLocation();
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
+
+  if (location.pathname === "/" || location.pathname === "/signup") {
+    return (
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1 }}
-        className="h-[20rem] w-full bg-neutral-900 text-neutral-500 pt-32 text-center"
+        className="w-full bg-neutral-900 text-neutral-500 py-32 text-center"
       >
-        <div className="text-neutral-200 text-4xl font-bold text-center justify-center">
+        <div className="text-neutral-200 text-4xl font-bold text-center justify-center mb-8">
           Made By Faizan.
         </div>
       </motion.div>
-    </Link>
+    );
+  }
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1 }}
+      className="w-full bg-neutral-900 text-neutral-500 py-32 text-center"
+    >
+      <div className="text-neutral-200 text-4xl font-bold text-center justify-center mb-8">
+        Made By Faizan.
+      </div>
+      <Link to="/" className="">
+        <button
+          onClick={handleClick}
+          className="text-2xl font-bold hover:px-20 transition-all duration-500 rounded-2xl bg-slate-50 px-8 py-3 text-gray-800 max-w-fit"
+        >
+          Logout
+        </button>
+      </Link>
+    </motion.div>
   );
 };
 
